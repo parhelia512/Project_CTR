e8f5f529c54ff9b22a2491a480ffa69206bf7b19
jakcron <jakcron.dev@gmail.com>
2026-01-18 13:13:02 +0800

    [makerom] Bump version to 0.19.0

--- makerom/src/version.h
+++ makerom/src/version.h
@@ -2,6 +2,6 @@
 #define APP_NAME	"CTR MAKEROM"
 #define BIN_NAME	"makerom"
 #define VER_MAJOR	0
-#define VER_MINOR	18
-#define VER_PATCH	5
+#define VER_MINOR	19
+#define VER_PATCH	0
 #define AUTHORS		"3DSGuy"
